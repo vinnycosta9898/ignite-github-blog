cc10df32b012f68e6b26cba48f5c39719342616e
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-06-08 11:38:31 -0300

    Criando o componente PostListContainer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
+import { PostListContainer } from "./styles";
 
 export function Home(){
     return(
         <div>
             <Profile/>
             <SearchInput/>
+            <PostListContainer>
+                
+            </PostListContainer>
         </div>
     )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PostListContainer = styled.section`
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 2rem;
+    margin-bottom: 14rem;
+
+`
